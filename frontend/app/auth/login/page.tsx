63b8f559411fb2ac0ad802caff411eb4fb677b80
misbahs100 <misbahuddins100@gmail.com>
2025-12-14 15:37:08 +0600

    login page

--- frontend/app/auth/login/page.tsx
+++ frontend/app/auth/login/page.tsx
@@ -40,7 +40,7 @@ export default function LoginPage() {
         <input value={password} onChange={e=>setPassword(e.target.value)} placeholder="Password" type="password" className="w-full p-2 border rounded"/>
         <button className="w-full bg-slate-800 text-white py-2 rounded">Login</button>
       </form>
-      <div className="mt-4 text-sm text-gray-600">Seed users: admin@local.test / Admin123! , manager@local.test / Manager123!, member@local.test / Member123!</div>
+      <div className="mt-4 text-sm text-gray-600">Users: <br />admin@local.test / Admin123! <br />manager@local.test / Manager123! <br /> member@local.test / Member123!</div>
     </div>
   );
 }
